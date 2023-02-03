9c5f5fcdbef059612d2c42d0910dde60d424c1e2
Tomáš Hobza <thomashobza@ids.company>
2023-02-03 13:58:32 +0100

    initial

---
+++ config/middlewares.js
@@ -0,0 +1,19 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+   {
+    name: 'strapi::compression',
+    config: {
+      br: false,
+      gzip: true,
+    },
+  },
+];
